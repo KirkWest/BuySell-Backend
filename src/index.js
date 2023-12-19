a3d8194827e1cba1af5979a0b1ff195b464d5c4b
KirkWest <126360978+KirkWest@users.noreply.github.com>
2023-12-19 16:20:32 +1100

    feature of boilerplate init runs in localhost

---
+++ src/index.js
@@ -0,0 +1,12 @@
+// Handles boot up of the server
+
+require('dotenv').config();
+
+const { databaseConnect } = require('./database');
+const { app } = require('./server');
+
+
+app.listen(3000, async () => {
+  await databaseConnect();
+  console.log("Server is running");
+});
