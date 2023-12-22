6bc24f551d46e22fa640f3488248c49380b680a6
KirkWest <126360978+KirkWest@users.noreply.github.com>
2023-12-22 16:38:02 +1100

    feat: Post request implemented

--- src/server.js
+++ src/server.js
@@ -11,6 +11,25 @@ app.get("/", (request, response) => {
 	});
 });
 
+app.post("/", (request, response) => {
+  response.json({
+    received: request.body.message
+  });
+});
+
+app.delete("/buy/:id", (request, response) => {
+	const resourceID = request.params.id;
+	response.json({
+		message:"DELETE request successful for ID $(buyID)"
+	});
+});
+
+app.patch("/", (request, response) => {
+	response.json({
+		message:"Update successful"
+	});
+});
+
 module.exports = {
 	app
 }
