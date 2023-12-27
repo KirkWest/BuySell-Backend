a677b3eb8bd54e7afb088cf14887b10c32043a5c
KirkWest <126360978+KirkWest@users.noreply.github.com>
2023-12-27 12:08:32 +1100

    feat: calendarEvent model built

---
+++ src/models/CalendarEvent.js
@@ -0,0 +1,22 @@
+const mongoose = require('mongoose');
+
+const Schema = mongoose.Schema;
+
+const CalendarEventSchema = new Schema({
+  date: {
+    type: Date,
+    required: true,
+  },
+  description: {
+    type: String,
+    required: true,
+  },
+  hasBuyButton: {
+    type: Boolean,
+    default: false,
+  },
+});
+
+const CalendarEvent = mongoose.model('CalendarEvent', CalendarEventSchema);
+
+module.exports = CalendarEvent;
