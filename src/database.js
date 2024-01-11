ae41ee31e79c76f5f2469c1805cf172b341ccb5e
KirkWest <126360978+KirkWest@users.noreply.github.com>
2024-01-11 17:57:30 +1100

    feat: updated database to correct  level

--- src/database.js
+++ src/database.js
@@ -5,7 +5,9 @@ const mongoose = require('mongoose');
 async function databaseConnect(){
 	try { 
 		console.log("Connecting to:\n" + process.env.DB_URI);
-		await mongoose.connect(process.env.DB_URI);
+		await mongoose.connect(process.env.DB_URI, {
+			dbName: 'DaySale',
+		});
 		console.log("Database connected");
 	} catch (error) {
 		console.warn(`databaseConnect failed to connect to DB:\n${JSON.stringify(error)}`);
