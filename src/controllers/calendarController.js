26e1ea5fe27713187c91b6f5c85e3d3295bb34aa
KirkWest <126360978+KirkWest@users.noreply.github.com>
2024-01-13 14:13:01 +1100

    feat: added in more validation and some restructuring

--- src/controllers/calendarController.js
+++ src/controllers/calendarController.js
@@ -5,35 +5,48 @@ exports.manageChildNames = async (req, res) => {
   const {date, action, childName} = req.body;
 
   try {
-    let event = await CalendarEvent.findOne({date});
+    let event = await CalendarEvent.findOne({ date });
 
     if (action == "add") {
       if (!event) {
         event = new CalendarEvent({ date, childNames: [childName] });
       } else {
-        event.childNames.push(childName);
+        // this prevents duplicate names (shouldn't really be an issue but best practice to have)
+        if (!event.childNames.includes(childName)) {
+          event.childNames.push(childName);
+        }
       }
     } else if (action === "remove") {
-      event.childNames = event.childNames.filter(name => name !== childName);
-      if (event.childNames.length == 0) {
-        await CalendarEvent.deleteOne({ _id: event._id });
+      if (event) {
+        event.childNames = event.childNames.filter(name => name !== childName);
+        if (event.childNames.length === 0) {
+          await CalendarEvent.deleteOne({ _id: event._id });
+        } else {
+          await event.save();
+        }
         return res.json({
           message: "Successfully removed child"
+        });
+      } else {
+        return res.status(404).json({
+          message: "Event not found"
         });
       }
     }
 
-    await event.save();
-    res.json({
-      message: "Day updated successfully"
-    });
+    if (event) {
+      await event.save();
+      res.json({
+       message: "Day updated successfully"
+      });
+    }
   } catch (error) {
     console.error(error);
     res.status(500).json({
       message: "Internal Server Error"
     });
   }
-}
+};
 
 // this will fetch from the database only when a buy button is active
 exports.getEvents = async (req, res) => {
