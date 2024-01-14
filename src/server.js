d3af0fed123590f7368a4591f6e63ece98a72a21
KirkWest <126360978+KirkWest@users.noreply.github.com>
2024-01-14 16:27:32 +1100

    removed old email system

--- src/server.js
+++ src/server.js
@@ -1,11 +1,11 @@
 const express = require('express');
 const authRouter = require('./routes/authRouter');
 const calendarRouter = require('./routes/calendarRouter.js');
-const emailRouter = require('./routes/emailRouter.js');
 
 // make a server instance 
 const app = express();
 
+// cors for cross platform
 const cors = require('cors');
 app.use(cors());
 
@@ -16,9 +16,6 @@ app.use('/auth', authRouter);
 
 // calendar route
 app.use('/calendar', calendarRouter);
-
-// guest email route
-app.use('/email', emailRouter);
 
 module.exports = {
 	app
